6020a676a7c69a019fc7930f1f204a967e8a070d
Andy Buske <buskeandrewt@johndeere.com>
2021-08-21 21:16:48 -0500

    add Giver Index and New, views and routes

--- controllers/giversController.js
+++ controllers/giversController.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require('../models').Giver;
+const Giver = require('../models').Giver;
 
 
+//Index Route
+router.get('/', (req, res) => {
+    Giver.findAll().then((foundGiver) => {
+        res.render('givers/index.ejs', {
+            giver: foundGiver,
+        });
+    });
+});
 
+//New Giver Routes
+router.get('/new', (req, res) => {
+    res.render('givers/newgiver.ejs');
+});
 
+router.post('/', (req, res) => {
+    Giver.create(req.body).then(() => {
+        res.redirect('/givers');
+    });
+});
 
 
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const User = require('../models').User;
+const Giver = require('../models').Giver;
 
 
 //Index Route
